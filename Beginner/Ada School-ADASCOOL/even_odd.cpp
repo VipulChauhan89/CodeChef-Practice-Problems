749fa8c89a3f795cfe8b109c19500d8efbf5a6b1
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-02-18 17:06:13 +0530

    Create even_odd.cpp

--- Beginner/Ada School-ADASCOOL/even_odd.cpp
+++ Beginner/Ada School-ADASCOOL/even_odd.cpp
@@ -1,3 +1,14 @@
+/*
+	Ada's classroom contains N⋅M tables distributed in a grid with N rows and M columns. 
+	Each table is occupied by exactly one student.
+
+	Before starting the class, the teacher decided to shuffle the students a bit. 
+	After the shuffling, each table should be occupied by exactly one student again. 
+	In addition, each student should occupy a table that is adjacent to that student's original table, 
+	i.e. immediately to the left, right, top or bottom of that table.
+
+	Program to find out that is it possible for the students to shuffle while satisfying all conditions of the teacher?
+*/
 #include <iostream>
 using namespace std;
 
