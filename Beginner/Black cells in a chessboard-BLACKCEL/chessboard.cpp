d08eda0bc327044681614d71300cfd936bac490d
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-03-05 22:05:47 +0530

    Create chessboard.cpp

---
+++ Beginner/Black cells in a chessboard-BLACKCEL/chessboard.cpp
@@ -0,0 +1,13 @@
+/*
+  Given n (n is even), determine the number of black cells in an n×n chessboard.
+*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n;
+	cin>>n;
+	cout<<(n*n)/2<<endl;
+	return 0;
+}
