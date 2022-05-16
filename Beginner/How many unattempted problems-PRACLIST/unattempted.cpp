0b67e3334aa0cbdd1a6fc902cf9714c72ef819db
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-05-16 16:43:19 +0530

    Create unattempted.cpp

---
+++ Beginner/How many unattempted problems-PRACLIST/unattempted.cpp
@@ -0,0 +1,14 @@
+/*
+  Our Chef is currently practicing on CodeChef and is a beginner. The count of ‘All Problems’ in the Beginner section is X. 
+  Our Chef has already ‘Attempted’ Y problems among them. How many problems are yet ‘Un-attempte
+*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int X,Y;
+	cin>>X>>Y;
+	cout<<X-Y<<endl;
+	return 0;
+}
