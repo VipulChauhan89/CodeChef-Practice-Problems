43c71ffe5806c7112b5f3d45e69bc5c8dc6e945c
VipulChauhan89 <90852928+VipulChauhan89@users.noreply.github.com>
2022-06-05 21:08:17 +0530

    Create second_max.cpp

---
+++ Beginner/Second Max of Three Numbers-SNDMAX/second_max.cpp
@@ -0,0 +1,20 @@
+/*
+  Write a program that accepts sets of three numbers, and prints the second-maximum number among the three.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,A,B,C;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>A>>B>>C;
+	    cout<<A+B+C-(min({A,B,C})+max({A,B,C}))<<endl;
+	}
+	return 0;
+}
