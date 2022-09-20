aa4fe5bb79f5be357f32630ae0b894a59f4efdde
VipulChauhan89 <vipulchauhan548@gmail.com>
2022-09-20 19:56:05 +0530

    Create bank.cpp

--- Beginner/Bob at the Bank-BOBBANK/bank.cpp
+++ Beginner/Bob at the Bank-BOBBANK/bank.cpp
@@ -1,3 +1,9 @@
+/*
+    Bob has an account in the Bobby Bank. His current account balance is W rupees.
+    Each month, the office in which Bob works deposits a fixed amount of X rupees to his account.
+    Y rupees is deducted from Bob's account each month as bank charges.
+    Find his final account balance after Z months. Note that the account balance can be negative as well.
+*/
 #include <iostream>
 using namespace std;
 
