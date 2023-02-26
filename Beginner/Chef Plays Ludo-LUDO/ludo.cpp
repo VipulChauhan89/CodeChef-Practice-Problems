21175ce17423a96c43b13c0728d03349b34a04f8
VipulChauhan89 <vipulchauhan548@gmail.com>
2023-02-26 11:11:19 +0530

    Create ludo.cpp

---
+++ Beginner/Chef Plays Ludo-LUDO/ludo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    (X==6)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	}
+	return 0;
+}
