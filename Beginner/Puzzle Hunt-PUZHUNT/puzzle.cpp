1477411a1e1626768a0a9b70ddda53c83b838c6a
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-08-25 23:00:24 +0530

    Create puzzle.cpp

---
+++ Beginner/Puzzle Hunt-PUZHUNT/puzzle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+	int N;
+	cin>>N;
+	(N>=6 && N<=8)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	
+	return 0;
+}
