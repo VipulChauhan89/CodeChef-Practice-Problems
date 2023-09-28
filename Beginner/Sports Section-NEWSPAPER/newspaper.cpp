0b2875a84a32098d63c746ae9c68f36bec6f3aa5
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2023-09-28 22:42:59 +0530

    Create newspaper.cpp

---
+++ Beginner/Sports Section-NEWSPAPER/newspaper.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int X;
+	cin>>X;
+	(X>7 && X<=10)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	return 0;
+}
