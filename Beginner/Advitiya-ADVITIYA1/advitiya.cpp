e8fc0dc78ba0029782c1d1b1a941d06e29a7e336
Vipul Chauhan <90852928+VipulChauhan89@users.noreply.github.com>
2024-02-01 19:56:00 +0530

    Create advitiya.cpp

---
+++ Beginner/Advitiya-ADVITIYA1/advitiya.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int N;
+	cin>>N;
+	cout<<((N<16)?"WAITING FOR ADVITIYA":"ADVITIYA")<<endl;
+	return 0;
+}
