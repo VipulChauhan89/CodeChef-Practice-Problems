dbbc6ffd3636b108450688fbc6d0735a96fe6058
VipulChauhan89 <Vipulchauhan548@gmail.com>
2024-06-08 09:10:17 +0530

    Created cricket.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Cricket-World-Cup-Qualifier/cricket.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int X;
+	cin>>X;
+	cout<<((X>=12)?"Yes":"No")<<endl;
+	return 0;
+}
