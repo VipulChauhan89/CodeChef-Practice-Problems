810467d523afd87c85bfc893ad525039039a007b
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-10 23:42:03 +0530

    Created taxes.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Saving-Taxes/taxes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X,Y;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X>>Y;
+	    cout<<X-Y<<endl;
+	}
+	return 0;
+}
