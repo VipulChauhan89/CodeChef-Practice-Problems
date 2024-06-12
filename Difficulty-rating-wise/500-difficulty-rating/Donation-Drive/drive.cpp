348a19eab184238ec51fe458f7594093760b9d61
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-12 16:31:00 +0530

    Created drive.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Donation-Drive/drive.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,N,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N>>X;
+	    cout<<N-X<<endl;
+	}
+	return 0;
+}
