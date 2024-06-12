64b677f7101fc32820e203c58a2b868e60cf9657
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-12 16:29:14 +0530

    Created one.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Off-By-One/one.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int A,B;
+	cin>>A>>B;
+	cout<<A+B<<"1"<<endl;
+	return 0;
+}
