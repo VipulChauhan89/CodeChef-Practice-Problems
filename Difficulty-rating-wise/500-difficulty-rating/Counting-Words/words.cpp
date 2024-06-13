22eb13f8e9e13e1c4e60669a3c7270c425a21666
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-13 23:10:09 +0530

    Created words.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Counting-Words/words.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,N,M;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N>>M;
+	    cout<<N*M<<endl;
+	}
+	return 0;
+}
