62845df381be8254d1cc5f6b380937dcbb2c03d0
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-16 15:12:32 +0530

    Created quality.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Air-Quality-Index/quality.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int X;
+	cin>>X;
+	cout<<((X<100)?"YES":"NO")<<endl;
+	return 0;
+}
