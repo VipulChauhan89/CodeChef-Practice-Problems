62396defdc1c47f27ad0a5dba0b968ef2e3b132d
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-17 20:47:34 +0530

    Created trip.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Car-Trip/trip.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    cout<<((X>300)?(3000+(X-300)*10):3000)<<endl;
+	}
+	return 0;
+}
