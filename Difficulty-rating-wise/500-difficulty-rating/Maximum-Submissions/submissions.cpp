e3b8e05cbb8534b7fbad18451f546aa0aae73668
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-19 21:09:15 +0530

    Created submissions.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Maximum-Submissions/submissions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    cout<<2*X<<endl;
+	}
+	return 0;
+}
