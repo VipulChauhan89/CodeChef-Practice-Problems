c171cd58c5095e266d66315e50b45e4d41508c23
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-21 23:58:56 +0530

    Created iq.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Increase-IQ/iq.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int X;
+	cin>>X;
+	cout<<((X+7>170)?"Yes":"No")<<endl;
+	return 0;
+}
