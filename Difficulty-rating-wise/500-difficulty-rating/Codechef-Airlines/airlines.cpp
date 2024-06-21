3db606e70416e3c5cbf73f21ee52a63d1568a672
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-21 23:56:46 +0530

    Created airlines.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Codechef-Airlines/airlines.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X,Y,Z;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X>>Y>>Z;
+	    cout<<min(X*10,Y)*Z<<endl;
+	}
+	return 0;
+}
