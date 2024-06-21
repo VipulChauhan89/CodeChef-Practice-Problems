abd2e9b65fb5296ea24ff7259d2a4a391c3ac27a
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-21 23:48:37 +0530

    Created insurance.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Insurance/insurance.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X,Y;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X>>Y;
+	    cout<<min(X,Y)<<endl;
+	}
+	return 0;
+}
