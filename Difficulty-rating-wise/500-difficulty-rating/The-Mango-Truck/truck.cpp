bfdf260b68971787a29024148034f9e93da4541c
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-22 22:42:07 +0530

    Created truck.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/The-Mango-Truck/truck.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X,Y,Z;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X>>Y>>Z;
+	    cout<<((Z-Y)/X)<<endl;
+	}
+	return 0;
+}
