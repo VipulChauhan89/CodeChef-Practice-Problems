29349764414c4f067194b1726fbd58494c14bf9c
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-23 22:58:58 +0530

    Created coupon.cpp

---
+++ Difficulty-rating-wise/500-difficulty-rating/Best-Coupon/coupon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X;
+	    cout<<((X>1000)?X/10:100)<<endl;
+	}
+	return 0;
+}
