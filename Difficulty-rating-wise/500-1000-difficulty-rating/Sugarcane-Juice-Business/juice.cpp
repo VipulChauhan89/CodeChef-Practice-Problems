20b81f080ff01a305f1ff25475dd47c767708a1b
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-25 23:13:34 +0530

    Created juice.cpp

---
+++ Difficulty-rating-wise/500-1000-difficulty-rating/Sugarcane-Juice-Business/juice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,N;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N;
+	    cout<<N*15<<endl;
+	}
+	return 0;
+}
