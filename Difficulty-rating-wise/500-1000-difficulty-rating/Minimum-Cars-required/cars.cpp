fbc247f13f10a187d0a149563b5d1f6c375a2949
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-28 23:38:26 +0530

    Created cars.cpp

---
+++ Difficulty-rating-wise/500-1000-difficulty-rating/Minimum-Cars-required/cars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T;
+	double N;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N;
+	    cout<<ceil(N/4)<<endl;
+	}
+	return 0;
+}
