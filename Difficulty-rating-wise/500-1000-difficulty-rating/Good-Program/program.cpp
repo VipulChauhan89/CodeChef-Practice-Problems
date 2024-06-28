0a171d72f47e28b40838ea137dcb88e11d8cb166
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-28 23:06:54 +0530

    Created program.cpp

---
+++ Difficulty-rating-wise/500-1000-difficulty-rating/Good-Program/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,N;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N;
+	    cout<<((N%4==0)?"Good":"Not Good")<<endl;
+	}
+	return 0;
+}
