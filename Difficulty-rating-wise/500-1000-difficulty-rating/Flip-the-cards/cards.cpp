a2ccc3f465c9dbe7776d87119a3ecdf5fd22b800
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-30 22:36:53 +0530

    Created cards.cpp

---
+++ Difficulty-rating-wise/500-1000-difficulty-rating/Flip-the-cards/cards.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,N,X;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N>>X;
+	    cout<<min(N-X,X)<<endl;
+	}
+	return 0;
+}
