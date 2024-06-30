bc434e387d7969e11f1a634549df6149e9567bb4
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-06-30 22:20:26 +0530

    Created police.cpp

---
+++ Difficulty-rating-wise/500-1000-difficulty-rating/Police-and-Thief/police.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,X,Y;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>X>>Y;
+	    cout<<abs(X-Y)<<endl;
+	}
+	return 0;
+}
