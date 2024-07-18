1b18d27d9bc306d41080f75c75c92325018d6707
Vipulchauhan89 <vipulchauhan548@gmail.com>
2024-07-18 22:20:24 +0530

    Created dracula.cpp

---
+++ Difficulty-rating-wise/500-1000-difficulty-rating/Dracula-Eats/dracula.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int T,N;
+	cin>>T;
+	while(T--)
+	{
+	    cin>>N;
+	    N+=5;
+	    cout<<N/7<<endl;
+	}
+	return 0;
+}
